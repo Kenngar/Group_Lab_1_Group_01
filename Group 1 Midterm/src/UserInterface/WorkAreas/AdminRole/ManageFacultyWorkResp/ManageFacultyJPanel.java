9f963a6f01801db583f4cd187511cb191ac2c9a2
126anguyen <nguyen.a12@northeastern.edu>
2026-06-28 13:22:07 -0700

    Test and adjust logout button

--- Group 1 Midterm/src/UserInterface/WorkAreas/AdminRole/ManageFacultyWorkResp/ManageFacultyJPanel.java
+++ Group 1 Midterm/src/UserInterface/WorkAreas/AdminRole/ManageFacultyWorkResp/ManageFacultyJPanel.java
@@ -221,6 +221,7 @@ public class ManageFacultyJPanel extends javax.swing.JPanel {
         }
         selected = rowProfiles.get(row);
         txtName.setText(selected.getPerson().getName());
+        txtTitle.setText(selected.getTitle());
 
     }//GEN-LAST:event_tblFacultyMousePressed
 
